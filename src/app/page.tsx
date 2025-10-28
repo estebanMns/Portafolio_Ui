a12cbf98b6c1ef7ea504fcce032dfccdf75349da
estebanMns <esteban.meneses@campusucc.edu.co>
2025-10-27 23:32:11 -0500

    subiendo acutlizacion del carrusel

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -23,9 +23,8 @@ export default function Home() {
           <img src="./images/avatar.png" 
           alt="Avatar" 
           className="absolute -top-10 -left-10 w-[265px] h-[276px] rounded-[25px] border-2 border-[#3a0d0d] shadow-xl opacity-90 object-cover bg-[#d1d8db]" />
-        
-
-          <div className="flex flex-col items-start justify-end text-white space-y-2 p-80 ">
+         
+          <div className="flex flex-col items-start justify-end text-white space-y-2 p-80 "> 
             <h1 className=" text-[50px] leading-none">Esteban</h1>
             <h1 className=" text-[50px] leading-none">Meneses</h1>
             <h3 className=" text-[30px] x-20">zlesteban1008@gmail.com</h3>
